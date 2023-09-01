9f0ca3238e95f2d05df08cb4b82477db316106b2
Tatu Saloranta <tatu.saloranta@datastax.com>
2023-09-01 14:00:02 -0700

    Try creating trivial service impl

---
+++ src/main/java/com/datastax/vector/VectorApiService.java
@@ -0,0 +1,22 @@
+package com.datastax.vector;
+
+import io.quarkus.grpc.GrpcService;
+import io.smallrye.mutiny.Uni;
+
+import java.util.Arrays;
+import java.util.List;
+
+@GrpcService
+public class VectorApiService implements VectorApi {
+    @Override
+    public Uni<VectorResponse> vectorize(VectorRequest request) {
+        String contents = request.getContents();
+        List<String> tokens = Arrays.asList(contents.split("\\s+"));
+        VectorResponse response = VectorResponse.newBuilder()
+                .setContents(contents)
+                .addAllTokens(tokens)
+                .setCount(tokens.size())
+                .build();
+        return Uni.createFrom().item(response);
+    }
+}
